b110e3148d6ca0ee00e35707cfa96560e33b0572
Abdul Baset Bappy <abdulbasetbappy@hotmail.com>
2024-03-19 16:46:27 +0600

    Login Page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	
+	postcss: {
+		plugins: {
+		  "postcss-custom-properties": false
+		},
+	  },
 	modules: [
 		'@nuxtjs/tailwindcss',
 		'nuxt-primevue'
