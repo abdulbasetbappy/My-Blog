d4d637b57a8707c555495dff6d47e34749f2bf5d
Ahsan855 <md.ahsanhabib9869@gmail.com>
2024-03-29 09:55:37 +0600

    navbar done

---
+++ pages/pemrograman/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-20">
+    <h1>this is Pemrograman</h1>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "forntend",
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/teknologi/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-20">
+    <h1>this is Teknologi</h1>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "forntend",
+});
+</script>
+
+<style lang="scss" scoped></style>
