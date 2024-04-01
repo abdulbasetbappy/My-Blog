79ec2ab788515d5338ae21ead227610d7f31e82b
Ahsan855 <md.ahsanhabib9869@gmail.com>
2024-04-01 13:41:21 +0600

    added login register

--- components/layouts/frontend/Navbar.vue
+++ components/layouts/frontend/Navbar.vue
@@ -71,38 +71,8 @@ function toggleMobileMenu() {
         <div class="-mr-2 sm:block md:hidden flex items-center">
           <button @click="toggleMobileMenu" type="button" class="text-white">
             <span class="sr-only">Open main menu</span>
-            <svg
-              v-if="!mobileMenuOpen"
-              class="block h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-            <svg
-              v-if="mobileMenuOpen"
-              class="block h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+            <Icon v-if="!mobileMenuOpen" name="heroicons:bars-3"></Icon>
+            <Icon v-if="mobileMenuOpen" name="maki:cross"></Icon>
           </button>
         </div>
       </div>
