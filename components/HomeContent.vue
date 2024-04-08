bb8d4f38bae383f9e0fa27658f2cd54fab14f0b9
Ahsan855 <md.ahsanhabib9869@gmail.com>
2024-04-08 10:21:47 +0600

    added console log

--- components/HomeContent.vue
+++ components/HomeContent.vue
@@ -22,17 +22,7 @@ const truncateDescription = (description) => {
   return description;
 };
 
-onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-});
 
-const handleScroll = () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
-  if (Math.ceil(scrolled) === scrollable) {
-    console.log("You've reached the bottom!");
-  }
-};
 </script>
 
 <template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,17 @@
 definePageMeta({
   layout: "forntend",
 });
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+const handleScroll = () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  if (Math.ceil(scrolled) === scrollable) {
+    console.log("You've reached the bottom!");
+  }
+};
 </script>
 <template>
   <HomeContent />
